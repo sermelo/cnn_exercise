663f7f6dcb8c62962351ba1431e7bcbf77bead16
Sergio Merino <sergio.merino@youview.com>
2021-09-21 18:49:07 +0100

    As the code is now in the docker image we don't need to mount it

--- start_ml_docker.sh
+++ start_ml_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 docker build docker/. -t vector
-docker run --rm -ti -v $PWD:/home/vector/workspace --entrypoint /bin/bash vector
+docker run --rm -ti --entrypoint /bin/bash vector
