34f267a43ce610672ffbb1e16c8079bcdc3bf775
Sergio Merino <sergio.merino@youview.com>
2021-09-22 09:10:09 +0100

    Remove sleep from entrypoint. The script will do active wait for kafka

--- docker/base/src/lib/publisher.py
+++ docker/base/src/lib/publisher.py
@@ -1,9 +1,26 @@
-from kafka import KafkaConsumer, KafkaProducer
+import time
+
+from kafka import KafkaProducer
+from kafka.errors import NoBrokersAvailable
 
 class Publisher():
     def __init__(self, topic, kafka_server='kafka'):
         self.topic = topic
-        self.producer = KafkaProducer(bootstrap_servers=kafka_server)
+        self.producer = self.get_producer(kafka_server)
+
+    def get_producer(self, server):
+        producer = None
+        for _ in range(4): # Try to connect 4 + 1 times
+            try:
+                producer = KafkaProducer(bootstrap_servers=server)
+            except NoBrokersAvailable:
+                time.sleep(1)
+                continue
+            else:
+                break
+        if not producer:
+            producer = KafkaProducer(bootstrap_servers=server)
+        return producer
 
     def send(self, message):
         meta_data = self.producer.send(self.topic, message)
--- docker/base/src/lib/subscriber.py
+++ docker/base/src/lib/subscriber.py
@@ -1,11 +1,30 @@
-from kafka import KafkaConsumer, KafkaProducer
+import time
+
+from kafka import KafkaConsumer
+from kafka.errors import NoBrokersAvailable
 
 class Subscriber():
     def __init__(self, topic, kafka_server='kafka'):
         self.topic = topic
-        self.consumer = KafkaConsumer(
-            self.topic,
-            bootstrap_servers=kafka_server)
+        self.consumer = self.get_consumer(kafka_server)
+
+    def get_consumer(self, server):
+        consumer = None
+        for _ in range(4): # Try to connect 4 + 1 times
+            try:
+                consumer = KafkaConsumer(
+                    self.topic,
+                    bootstrap_servers=server)
+            except NoBrokersAvailable:
+                time.sleep(1)
+                continue
+            else:
+                break
+        if not consumer:
+            consumer = KafkaConsumer(
+                self.topic,
+                bootstrap_servers=server)
+        return consumer
 
     def messages(self):
         while True:
